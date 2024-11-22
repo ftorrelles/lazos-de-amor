df8e212593c639221c3c5742a3bf87fd1d9eb556
ftorrelles <torrellesf93@gmai.com>
2024-11-22 15:35:49 -0500

    update component Creating awareness

--- src/assets/components/CreatingAwareness.jsx
+++ src/assets/components/CreatingAwareness.jsx
@@ -1,22 +1,46 @@
-import "../styles/creatingAwareness.css";
 import React from "react";
 import Slider from "react-slick";
+import "../styles/creatingAwareness.css";
+
+// Creamos componentes de flechas personalizados
+const CustomArrow = ({
+  currentSlide,
+  slideCount,
+  className,
+  style,
+  onClick,
+  direction,
+}) => (
+  <button
+    className={className}
+    style={{ ...style, display: "block", background: "transparent" }}
+    onClick={onClick}
+  >
+    {direction === "next" ? ">" : "<"}
+  </button>
+);
 
 const videosConsciencia = [
   {
-    titulo: "Mitos y Realidades de la Adicción",
+    titulo: "1ra entrega, Mitos y realidades adicciones",
     videoUrl:
       "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F1105138483850665%2F&show_text=false&width=560&t=0",
   },
-  {
-    titulo: "Cruda realidad del consumo de Drogas",
-    videoUrl:
-      "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F2255037708203769%2F&show_text=false&width=560&t=0",
-  },
   // {
-  //   titulo: "Estrategias de Superación",
-  //   videoUrl: "https://www.youtube.com/watch?v=opUySGrwwj8&t=143s",
+  //   titulo: "2da entrega, Comportamiento en adicciones",
+  //   videoUrl:
+  //     "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F1242010370437639%2F&show_text=false&width=560&t=0",
   // },
+  {
+    titulo: "3ra entrega, Síndrome de abstinencia",
+    videoUrl:
+      "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F3896610743956815%2F&show_text=false&width=560&t=0",
+  },
+  {
+    titulo: "4ta entrega, Señales de riesgo",
+    videoUrl:
+      "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F445283331628639%2F&show_text=false&width=560&t=0",
+  },
 ];
 
 const CreatingAwareness = () => {
@@ -27,9 +51,8 @@ const CreatingAwareness = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: false,
-
-    nextArrow: <button className="carousel-control next">⏩</button>,
-    prevArrow: <button className="carousel-control prev">⏪</button>,
+    nextArrow: <CustomArrow direction="next" />,
+    prevArrow: <CustomArrow direction="prev" />,
   };
 
   return (
