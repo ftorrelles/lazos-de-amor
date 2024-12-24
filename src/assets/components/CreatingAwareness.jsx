e113e102170f7a3c559f79a5a3477083fd5fdc8f
ftorrelles <torrellesf93@gmai.com>
2024-12-24 15:22:15 -0500

    changes in section CreatingAwareness

--- src/assets/components/CreatingAwareness.jsx
+++ src/assets/components/CreatingAwareness.jsx
@@ -1,16 +1,97 @@
+// import React from "react";
+// import Slider from "react-slick";
+// import "../styles/creatingAwareness.css";
+
+// // Creamos componentes de flechas personalizados
+// const CustomArrow = ({
+//   currentSlide,
+//   slideCount,
+//   className,
+//   style,
+//   onClick,
+//   direction,
+// }) => (
+//   <button
+//     className={className}
+//     style={{ ...style, display: "block", background: "transparent" }}
+//     onClick={onClick}
+//   >
+//     {direction === "next" ? ">" : "<"}
+//   </button>
+// );
+
+// const videosConsciencia = [
+//   {
+//     titulo: "10ma entrega, Éxtasis (MDMA)",
+//     videoUrl:
+//       "https://www.facebook.com/plugins/video.php?height=315&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F1233994907704951%2F&show_text=false&width=560&t=0",
+//   },
+//   {
+//     titulo: "9na entrega, Pegante",
+//     videoUrl:
+//       "https://www.facebook.com/plugins/video.php?height=315&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F607808115008629%2F&show_text=false&width=560&t=0",
+//   },
+//   {
+//     titulo: "8va entrega, Cocaína",
+//     videoUrl:
+//       "https://www.facebook.com/plugins/video.php?height=315&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F613241081029543%2F&show_text=false&width=560&t=0",
+//   },
+// ];
+
+// const CreatingAwareness = () => {
+//   const settings = {
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: false,
+//     nextArrow: <CustomArrow direction="next" />,
+//     prevArrow: <CustomArrow direction="prev" />,
+//   };
+
+//   return (
+//     <section className="consciencia-section">
+//       <h2 className="consciencia-title">Creando Conciencia</h2>
+//       <div className="breaking-news-frame">
+//         <Slider {...settings} className="video-carousel">
+//           {videosConsciencia.map((video, index) => (
+//             <div key={index} className="video-container">
+//               <h3 className="video-title">{video.titulo}</h3>
+//               {video.videoUrl.includes("facebook.com") ? (
+//                 <iframe
+//                   src={video.videoUrl}
+//                   width="100%"
+//                   height="100%"
+//                   style={{ border: "none", overflow: "hidden" }}
+//                   scrolling="no"
+//                   frameBorder="0"
+//                   allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+//                   allowFullScreen
+//                   className="testimonio-video"
+//                 ></iframe>
+//               ) : (
+//                 <video controls className="testimonio-video">
+//                   <source src={video.videoUrl} type="video/mp4" />
+//                   Tu navegador no soporta la etiqueta de video.
+//                 </video>
+//               )}
+//             </div>
+//           ))}
+//         </Slider>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default CreatingAwareness;
+
 import React from "react";
 import Slider from "react-slick";
 import "../styles/creatingAwareness.css";
 
 // Creamos componentes de flechas personalizados
-const CustomArrow = ({
-  currentSlide,
-  slideCount,
-  className,
-  style,
-  onClick,
-  direction,
-}) => (
+const CustomArrow = ({ className, style, onClick, direction }) => (
   <button
     className={className}
     style={{ ...style, display: "block", background: "transparent" }}
@@ -23,18 +104,15 @@ const CustomArrow = ({
 const videosConsciencia = [
   {
     titulo: "10ma entrega, Éxtasis (MDMA)",
-    videoUrl:
-      "https://www.facebook.com/plugins/video.php?height=315&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F1233994907704951%2F&show_text=false&width=560&t=0",
+    videoUrl: "https://www.youtube.com/embed/pKAAdf-03QM?si=goenaZRm2YdptgTI",
   },
   {
     titulo: "9na entrega, Pegante",
-    videoUrl:
-      "https://www.facebook.com/plugins/video.php?height=315&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F607808115008629%2F&show_text=false&width=560&t=0",
+    videoUrl: "https://www.youtube.com/embed/5-NJohCASaw?si=hAIe6cky7sdNk4Et",
   },
   {
     titulo: "8va entrega, Cocaína",
-    videoUrl:
-      "https://www.facebook.com/plugins/video.php?height=315&href=https%3A%2F%2Fwww.facebook.com%2F61559318943462%2Fvideos%2F613241081029543%2F&show_text=false&width=560&t=0",
+    videoUrl: "https://www.youtube.com/embed/Z8knbcpagH8?si=cDFGTaaSi244a0GS",
   },
 ];
 
@@ -48,6 +126,7 @@ const CreatingAwareness = () => {
     autoplay: false,
     nextArrow: <CustomArrow direction="next" />,
     prevArrow: <CustomArrow direction="prev" />,
+    lazyLoad: "ondemand",
   };
 
   return (
@@ -58,24 +137,18 @@ const CreatingAwareness = () => {
           {videosConsciencia.map((video, index) => (
             <div key={index} className="video-container">
               <h3 className="video-title">{video.titulo}</h3>
-              {video.videoUrl.includes("facebook.com") ? (
-                <iframe
-                  src={video.videoUrl}
-                  width="100%"
-                  height="100%"
-                  style={{ border: "none", overflow: "hidden" }}
-                  scrolling="no"
-                  frameBorder="0"
-                  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                  allowFullScreen
-                  className="testimonio-video"
-                ></iframe>
-              ) : (
-                <video controls className="testimonio-video">
-                  <source src={video.videoUrl} type="video/mp4" />
-                  Tu navegador no soporta la etiqueta de video.
-                </video>
-              )}
+              <iframe
+                src={video.videoUrl}
+                width="100%"
+                height="100%"
+                style={{ border: "none", overflow: "hidden" }}
+                scrolling="no"
+                frameBorder="0"
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                allowFullScreen
+                className="testimonio-video"
+                loading="lazy" // Este atributo mejora el rendimiento
+              ></iframe>
             </div>
           ))}
         </Slider>
